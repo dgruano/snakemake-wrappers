3af08152d6cc617680eb860987c40c8086572e10
Daniel Garcia Ruano <daniel.garciaruano@ibgc.cnrs.fr>
2025-06-11 16:17:15 +0200

    Adapt blastn wrapper to create blastp wrapper

---
+++ bio/blast/blastp/wrapper.py
@@ -0,0 +1,26 @@
+__author__ = "Daniel García-Ruano"
+__copyright__ = "Copyright 2025, Daniel García-Ruano"
+__email__ = "dgruano.code@gmail.com"
+__license__ = "MIT"
+
+from snakemake.shell import shell
+from os import path
+
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+
+format = snakemake.params.get("format", "")
+blastdb = snakemake.input.get("blastdb", "")[0]
+db_name = path.splitext(blastdb)[0]
+
+if format:
+    out_format = " -outfmt '{}'".format(format)
+
+shell(
+    "blastp"
+    " -query {snakemake.input.query}"
+    " {out_format}"
+    " {snakemake.params.extra}"
+    " -db {db_name}"
+    " -num_threads {snakemake.threads}"
+    " -out {snakemake.output[0]}"
+)
